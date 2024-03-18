41e20dfdeb9367f3333a03ed1d3085af49c4a5be
iss06 <iss06@naver.com>
2024-03-18 22:30:20 +0900

    Practice03

---
+++ src/com/javaex/practice/Ex04.java
@@ -0,0 +1,40 @@
+package com.javaex.practice;
+
+public class Ex04 {
+	//	아래의 출력결과를 예상하여 작성하세요.
+	//	(코드를 작성해서 예상과 맞는지 확인해 보세요.)
+	public static void main(String[] args) {
+		
+		for(int i=1; i<5; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("=====================");
+		
+		for(int i=10; i>0; i=i-1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
+
+/*
+
+1
+2
+3
+4
+=====================
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+*/
---
+++ src/com/javaex/practice/Ex07.java
@@ -0,0 +1,21 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex07 {
+	//	숫자를 입력 받아 아래와 같이 출력하세요.
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요.");
+		int num = scanner.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
